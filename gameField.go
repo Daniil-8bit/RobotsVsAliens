2798155b6f50d46a7fa5bcbdc81fbc48def5e0f4
Daniil-8bit <danshin.daniil@gmail.com>
2025-07-03 22:14:02 +0300

    add show warriors info

--- gameField.go
+++ gameField.go
@@ -1,6 +1,25 @@
 package main
 
+import "fmt"
+
 type GameField struct {
 	width  int
 	length int
+}
+
+var robotsPositions []Robot
+var aliansPositions []Alien
+
+func showRobotsPositions(r []Robot) {
+
+	for k, v := range r {
+		fmt.Printf("Robot%d: %s\nHealth: %d\nPosition on the field: (%d;%d)\n\n", k+1, v.name, v.health, v.x, v.y)
+	}
+}
+
+func showAliensPositions(a []Alien) {
+
+	for k, v := range a {
+		fmt.Printf("Alien%d: %s\nHealth: %d\nPosition on the field: (%d;%d)\n\n", k+1, v.name, v.health, v.x, v.y)
+	}
 }
--- main.go
+++ main.go
@@ -2,4 +2,21 @@ package main
 
 func main() {
 
+	robotsAmount := make([]Robot, 0)
+	aliensAmount := make([]Alien, 0)
+
+	robotsAmount = []Robot{
+		Robot{name: "Small Robot", health: 30, Location: Location{x: 7, y: 1}},
+		Robot{name: "Middle Robot", health: 50, Location: Location{x: 5, y: 3}},
+		Robot{name: "Big Robot", health: 90, Location: Location{x: 6, y: 2}},
+	}
+
+	aliensAmount = []Alien{
+		Alien{name: "Small Aien", health: 45, Location: Location{x: 1, y: 5}},
+		Alien{name: "Middle Aien", health: 20, Location: Location{x: 3, y: 1}},
+		Alien{name: "Big Aien", health: 75, Location: Location{x: 2, y: 3}},
+	}
+
+	showRobotsPositions(robotsAmount)
+	showAliensPositions(aliensAmount)
 }
